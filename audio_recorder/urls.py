6d459efc8419127610d08dbd6327b89cdee1e5ea
Baejjyeee <bjj3141592@naver.com>
2023-11-26 21:15:41 +0900

    record통신

--- audio_recorder/urls.py
+++ audio_recorder/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-	path('record/', indexView.as_view(), name="audio_recorder"),
+	path('record/', AudioFileCreateViewMixin.as_view(), name="audio_recorder"),
 ]
--- audio_recorder/views.py
+++ audio_recorder/views.py
@@ -1,7 +1,9 @@
 from django import http
 from django.views.generic.base import View
 from django.shortcuts import render, redirect, HttpResponse
-
+from django.http import JsonResponse
+from django.views import View
+import os
 class indexView(View):
     def get(self, request):
         return render(request, 'record.html')
@@ -12,15 +14,34 @@ class AudioFileCreateViewMixin(View):
     def create_object(self, audio_file):
         return self.model.objects.create(**{self.create_field: audio_file})
 
-    def post(self, request):
+    # def post(self, request):
+    #     audio_file = request.FILES.get('audio_file', None)
+    #     print("asdasd")
+    #     print(audio_file)
+    #     if audio_file is None:
+    #         return http.HttpResponseBadRequest()
+    #     result = self.create_object(audio_file)
+    #     print(result.audio_file.url)
+    #     return http.JsonResponse({
+    #         'id': result.pk,
+    #         'url': result.audio_file.url,
+    #     }, status=201)
+
+    def post(self, request, *args, **kwargs):
         audio_file = request.FILES.get('audio_file', None)
-
         if audio_file is None:
-            return http.HttpResponseBadRequest()
+            return JsonResponse({'error': 'No audio file provided'}, status=400)
 
-        result = self.create_object(audio_file)
-        print(result.audio_file.url)
-        return http.JsonResponse({
-            'id': result.pk,
-            'url': result.audio_file.url,
-        }, status=201)
+        # 여기서 파일을 저장하거나 다른 처리를 수행합니다.
+        # 예를 들어, 저장된 파일의 경로를 사용하여 응답을 생성할 수 있습니다.
+        #file_path = '/path/to/uploaded/file'  # 여기에 실제 파일 경로를 넣어주세요.
+        current_directory = os.path.dirname(os.path.abspath(__file__))
+        processed_file_path = os.path.join(current_directory, 'record_audio.wav')
+
+        print()
+        print("asdasdasdfasfnasfaiosdfnjaoijfo")
+        print()
+        print(processed_file_path)
+        print()
+        print()
+        return JsonResponse({'file_path': processed_file_path}, status=201)
