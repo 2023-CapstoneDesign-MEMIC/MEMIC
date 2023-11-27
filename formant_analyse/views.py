f4382129471b2c33256690c5e42f81227ad4f6a4
liljuice <juice@kakao.com>
2023-11-27 13:35:02 +0900

    중간 코드 #6: 피드백 구문 버전1

--- formant_analyse/views.py
+++ formant_analyse/views.py
@@ -65,24 +65,145 @@ def formant_vowel(fa):
     return result
 
 def feedback(sV, uV):
+    """
+    1. **Vowel Height (높이)**: Ranges from high (고모음) to low (저모음). It indicates how close the tongue is to the roof of the mouth.
+       - 0: Low
+       - 1: Near-low
+       - 2: Lower-mid
+       - 3: Mid
+       - 4: Upper-mid
+       - 5: Near-high
+       - 6: High
+
+    2. **Vowel Backness (기울기)**: Ranges from front (전설) to back (후설). It indicates the position of the tongue during the articulation of the vowel.
+       - 0: Back
+       - 1: Near-back
+       - 2: Central
+       - 3: Near-front
+       - 4: Front
+
+    3. **Lip Rounding (입술 모양)**: Indicates whether the lips are rounded or not.
+       - 0: Unrounded
+       - 1: Rounded
+
+    - 'a' (Open front unrounded vowel): [0, 4, 0]
+    - 'æ' (Near-open front unrounded vowel): [1, 4, 0]
+    - 'ɑ' (Open back unrounded vowel): [0, 0, 0]
+    - 'ɒ' (Open back rounded vowel): [0, 0, 1]
+    - 'e' (Close-mid front unrounded vowel): [4, 4, 0]
+    - 'ə' (Mid-central vowel): [3, 2, 0]
+    - 'ɛ' (Open-mid front unrounded vowel): [2, 4, 0]
+    - 'ɤ' (Close-mid back unrounded vowel): [4, 0, 0]
+    - 'i' (Close front unrounded vowel): [6, 4, 0]
+    - 'ɪ' (Near-close near-front unrounded vowel): [5, 3, 0]
+    - 'ɨ' (Close central unrounded vowel): [6, 2, 0]
+    - 'o' (Close-mid back rounded vowel): [4, 0, 1]
+    - 'ø' (Close-mid front rounded vowel): [4, 4, 1]
+    - 'œ' (Open-mid front rounded vowel): [2, 4, 1]
+    - 'ɔ' (Open-mid back rounded vowel): [2, 0, 1]
+    - 'ɵ' (Close-mid central rounded vowel): [4, 2, 1]
+    - 'u' (Close back rounded vowel): [6, 0, 1]
+    - 'ɯ' (Close back unrounded vowel): [6, 0, 0]
+    - 'ʊ' (Near-close near-back rounded vowel): [5, 1, 1]
+    - 'ʌ' (Open-mid back unrounded vowel): [2, 0, 0]
+    - 'y' (Close front rounded vowel): [6, 4, 1]
+    - 'ʏ' (Near-close near-front rounded vowel): [5, 3, 1]
+
+    s - u를 계산해서
+    일치하면 생략
+    +면 혀를 위로, -면 혀를 아래로
+    +면 혀를 앞으로, -면 혀를 뒤로
+    +면 입술을 오므리고, -면 입술을 펴고
+    """
+
     # vowel 종류 22가지
     classed = ['a', 'æ', 'ɑ', 'ɒ', 'e', 'ə', 'ɛ', 'ɤ', 'i', 'ɪ', 'ɨ',
                'o', 'ø', 'œ', 'ɔ', 'ɵ', 'u', 'ɯ', 'ʊ', 'ʌ', 'y', 'ʏ']
-    cnum = enumerate(classed)
+    # 높이 : 0 - 저모음, 1 - 근저모음, 2 - 중저모음, 3 - 중모음, 4 - 중고모음, 5 - 근고모음, 6 - 고모음
+    # 기울기 : 0 - 후설, 1 - 근후설, 2 - 중설, 3 - 근전설, 4 - 전설
+    # 입술 모양 : 0 - 평순, 1 - 원순
 
-    sentence = ""
-    sentence += "따라하려는 음성은 " + str(sV) + " 발음입니다." + '\n'
-    sentence += "사용자님의 음성은 " + str(uV) + " 발음입니다."
+    # 혀와 입술 모양 enum 배열으로 매핑
+    # tongue_height, tongue_slope, lip_shape
+    # tongue_height[uv[mapped_vowels]] << 피드백 구문 호출
+    mapped_vowels = {
+        'a': [0, 4, 0], 'æ': [1, 4, 0], 'ɑ': [0, 0, 0], 'ɒ': [0, 0, 1],
+        'e': [4, 4, 0], 'ə': [3, 2, 0], 'ɛ': [2, 4, 0], 'ɤ': [4, 0, 0],
+        'i': [6, 4, 0], 'ɪ': [5, 3, 0], 'ɨ': [6, 2, 0], 'o': [4, 0, 1],
+        'ø': [4, 4, 1], 'œ': [2, 4, 1], 'ɔ': [2, 0, 1], 'ɵ': [4, 2, 1],
+        'u': [6, 0, 1], 'ɯ': [6, 0, 0], 'ʊ': [5, 1, 1], 'ʌ': [2, 0, 0],
+        'y': [6, 4, 1], 'ʏ': [5, 3, 1]
+    }
 
-    if sV == uV:
-        sentence = ("따라하려는 음성과 사용자님의 음성의 모음 발음이 일치해요! "
-                    + "이 부분에서 점수를 높이려면, 목소리의 굵기와 크기를 조절해보세요.")
+    # [0]: ~으로 분류되며, [1]: ~ 위치하고
+    tongue_height = {
+        0: ['저모음', '혀의 높이를 매우 낮게'],
+        1: ['근저모음', '혀의 높이를 낮게'],
+        2: ['중저모음', '혀의 높이를 약간 낮게'],
+        3: ['중모음', '혀의 높이를 가운데'],
+        4: ['중고모음', '혀의 높이를 약간 높게'],
+        5: ['근고모음', '혀의 높이를 높게'],
+        6: ['고모음', '혀의 높이를 매우 높게']
+    }
 
-    for v in classed:
-        if v == sV:
-            sentence = ""
+    tongue_slope = {
+        0: ['후설모음', '혀끝의 위치를 매우 뒤 쪽(성대에 가깝게)에'],
+        1: ['근후설모음', '혀끝의 위치를 뒤 쪽(성대에 가깝게)에'],
+        2: ['중설모음', '혀끝의 위치를 가운데'],
+        3: ['근전설모음', '혀끝의 위치를 앞 쪽(입술에 가깝게)에'],
+        4: ['전설모음', '혀끝의 위치를 매우 앞 쪽(입술에 가깝게)에']
+    }
 
-    return sentence
+    lip_shape = {
+        0: ['평순모음', '입술을 오므리지 않고 평평한 모양으로'],
+        1: ['원순모음', '입술을 동그랗게 오므리고']
+    }
+
+    movement = ""
+    if mapped_vowels[sV][0] - mapped_vowels[uV][0] > 0: # 따라하려는 게 혀가 더 높다!
+        movement += "혀의 높이를 더 높게 올리고, "
+    elif mapped_vowels[sV][0] - mapped_vowels[uV][0] < 0:
+        movement += "혀의 높이를 더 낮게 내리고, "
+    else:
+        movement += "혀의 높이는 그대로 하고, "
+
+    if mapped_vowels[sV][1] - mapped_vowels[uV][1] > 0: # 따라하려는 게 혀가 더 앞쪽!
+        movement += "혀끝의 위치를 더 앞 쪽으로 옮기고, "
+    elif mapped_vowels[sV][1] - mapped_vowels[uV][1] < 0:
+        movement += "혀끝의 위치를 더 뒤 쪽으로 옮기고, "
+    else:
+        movement+= "혀끝의 위치는 그대로 하고, "
+
+    if mapped_vowels[sV][2] - mapped_vowels[uV][2] > 0:  # 따라하려는 게 원순 나는 평순
+        movement+= "입술을 오므려서 발음하세요."
+    elif mapped_vowels[sV][2] - mapped_vowels[uV][2] < 0:
+        movement+= "입술을 오므리지 않고 발음하세요."
+    else:
+        movement+= "입술의 모양은 그대로 하고 발음하세요."
+
+    sentence1 = ""
+    sentence1 += "따라하려는 음성은 " + sV + " 발음에 가깝고, "
+    sentence1 += "사용자님의 음성은 " + uV + " 발음에 가까워요.\n"
+
+    # 높이 / 기울기 / 입술 모양
+    sentence2 = ""
+
+    sentence2 += ("따라하려는 음성의 " + sV + "는 "
+                  + tongue_height[mapped_vowels[sV][0]][0] + "으로 분류되며, ")
+    sentence2 += tongue_height[mapped_vowels[sV][0]][1] + " 위치해야 하고, "
+    sentence2 += tongue_slope[mapped_vowels[sV][1]][0] + "으로 분류되며, "
+    sentence2 += tongue_slope[mapped_vowels[sV][1]][1] + " 위치해야 합니다.\n"
+
+    sentence2 += ("사용자님의 음성인 " + uV + "는 "
+                  + tongue_height[mapped_vowels[uV][0]][0] + "으로 분류되며, ")
+    sentence2 += tongue_height[mapped_vowels[uV][0]][1] + " 위치한 상태고, "
+    sentence2 += tongue_slope[mapped_vowels[uV][1]][0] + "으로 분류되며, "
+    sentence2 += tongue_slope[mapped_vowels[uV][1]][1] + " 위치한 상태입니다.\n"
+
+    sentence2 += "더 높은 점수를 얻으려면... "
+    sentence2 += movement
+
+    return sentence1, sentence2
 
 # 메인 함수
 def FormantAnalys(request):
@@ -118,7 +239,7 @@ def FormantAnalys(request):
     })
 
     idxnum = 0
-    print(similarity_ALL)
+    #print(similarity_ALL)
 
     for mapped in path:
         # DTW 결과에서 가져온 idx1 : sourceVocal.wav의 인덱스, idx2 : userVocal.wav의 인덱스
@@ -160,7 +281,7 @@ def FormantAnalys(request):
 
         df['score'] = mapped_score
         data = pd.concat([data, df])
-        print(mapped_score)
+        #print(mapped_score)
 
     # 결측값 제거
     data = data.dropna(axis=0)
@@ -198,7 +319,7 @@ def FormantAnalys(request):
         # "저희는 모음 포먼트 값을 기준으로 보고서를 제공합니다."
         # "점수를 높게 받았지만 만족스럽지 않다면, 목소리의 굵기나 목소리 크기 차이가 있기 때문입니다."
         # "모든 부분에서 점수가 높지 않더라도 훌륭한 성대모사가 될 수 있습니다. 따라하려는 목소리의 특징적인 부분만 높은 점수를 받아도 충분합니다."
-        feedback_sentence = feedback(sourceVowel[i], userVowel[i])
+        feedback_sentence, detail_sentence = feedback(sourceVowel[i], userVowel[i])
         print(feedback_sentence)
 
     # STT > 오류 발생 잦음... 특히 성대모사의 경우 발음이 뭉개지는 등의 경우가 많아
